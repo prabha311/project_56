218220f96d1ce0064fba75d8ae98da980263d73f
prabha311 <75113256+prabha311@users.noreply.github.com>
2021-02-03 12:47:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function changecolor(){
+    document.getElementById("heading1").style.color="yellow";
+}
+function setDate(){
+    document.getElementById("date").innerHTML = new Date().toDateString();
+}
